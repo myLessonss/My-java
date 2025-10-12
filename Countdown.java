2eef7d29567821c74837aab6be7f00d30d0df7df
Eloi <izabayoeloi2@gmail.com>
2025-10-12 10:47:41 +0200

    Adding Yatzy game

--- Countdown.java
+++ Countdown.java
@@ -1,12 +1,24 @@
 public class Countdown {
     public static void main(String [] args){
         int i=3;
+        int j=0;
         do{
             System.out.println(i);
             i--;
         }
         while(i>0);
         System.out.println("Happy day");
-    }
     
+    //Lets play "Yatzy" game we print Yatzy when dice print 6
+        
+        do{
+            j+=1;
+            if(j==6){
+            System.out.println("YATZY");
+           }else{
+            System.out.println("IT IS NOT YATZY");
+           }
+        }while(j<6);
+    }
 }
+
