8edcfe1b097563146bad5fe5525c77809f67e606
Eloi <izabayoeloi2@gmail.com>
2025-10-18 08:15:13 +0200

    Program for Findin Factorial

--- ForLoop/Factorial.java
+++ ForLoop/Factorial.java
@@ -8,14 +8,14 @@ public class Factorial {
         System.out.println("\nEnter in a number: ");
         number=myscan.nextInt();
         int fact=1;
-        int sum=0;
+        int prod=1;
         System.out.println("Finding Factorial of: "+number);
-        for(int i=number;i>0;--i){
-            fact=number*(i-1);
-            sum+=fact;
+        for(int i=number;i>=1;i--){
+            fact=(i);
+            prod=fact*prod;
             System.out.println(fact);
         }
-        System.out.println("Factorial of "+number+" : "+sum);
+        System.out.println("Factorial of "+number+" : "+prod);
 
     }
 }
