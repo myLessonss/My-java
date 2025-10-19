cfd4eab003385730d374348dd918cdaa81ab5e1d
Eloi <izabayoeloi2@gmail.com>
2025-10-19 05:56:46 +0200

    Program that print  numbers from 1 to 50

---
+++ WhileLoop/Print1to50.java
@@ -0,0 +1,12 @@
+package WhileLoop;
+//Print all numbers from 1 to 50 using while
+public class Print1to50 {
+    public static void main(String []args){
+        int number=1;
+        System.out.println("\nNumbers from 1 to 50");
+        while(number<=50){
+            System.out.println(number);
+            number++;
+        }
+    }
+}
