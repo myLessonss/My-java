c68523683624227565e7e6f6f156df1232f15d8e
Eloi <izabayoeloi2@gmail.com>
2025-10-21 17:53:37 +0200

    Program that compare reversals

---
+++ Methods/Reversals.java
@@ -0,0 +1,10 @@
+package Methods;
+/*Write a method compareReversals(int a, int b, boolean display) that:reverses both
+numbers,compares which reversed number is larger,prints both reversals if display
+is true,and returns the larger reversed value.  */
+public class Reversals {
+    public void compareReversals(int a, int b, boolean display){
+        
+    }
+    
+}
