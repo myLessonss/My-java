815842ca3ff7d8c7107a3d6f619e7a34401b941f
ELOI-CODE1 <izabayoeloi2@gmail.com>
2025-11-02 13:43:05 +0200

    update

---
+++ DoWhile/Test.java
@@ -0,0 +1,10 @@
+package DoWhile;
+public class Test{
+    public static void main(String[]args){
+        int i;
+        i=0;
+        do{
+            System.out.println("Hello world");
+        }while(i<3);
+    }
+}
