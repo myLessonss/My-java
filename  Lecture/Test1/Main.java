90727384d452ede8d2fea788edf31f187c682210
ELOI-CODE1 <izabayoeloi2@gmail.com>
2025-11-04 08:53:16 +0200

    Update

---  Lecture/Test1/Main.java
+++  Lecture/Test1/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String [] args){
         Scanner scn=new Scanner(System.in);
-        System.out.println("Enter in your id: ");
+        //Declare the object from class User
+        System.out.print("Enter in your id: "); 
         int id=scn.nextInt();
+        scn.nextLine();
+        System.out.print("Enter in your name: ");
+        String name=scn.nextLine();
+        User myobj=new User(id, name);
+        System.out.println("The user deatils are:\n Id:"+id+" \nName:"+name);
         System.out.println("Enter in your approved amount: ");
         double approvedAmount=scn.nextDouble();
         System.out.println("Enter in the amount of loan u want: ");
