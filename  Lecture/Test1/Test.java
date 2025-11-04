8b311da653096111764ed4864313a260a1e30220
ELOI-CODE1 <izabayoeloi2@gmail.com>
2025-11-04 08:17:12 +0200

    Constructor test

---  Lecture/Test1/Test.java
+++  Lecture/Test1/Test.java
@@ -1,5 +1,17 @@
 public class Test {
+    int a;
+    String name;
+   
+    public Test(String n,int age){
+        name=n;
+        a=age;
+    }
+    void display(){
+        System.out.println(name+" You are "+a+" years old.");
+    }
     public static void main(String[]args){
-        System.out.println("Hello");
+        Test obj=new Test("Eloi",23);
+        obj.display();
+
     }
 }
