dd3c075bf1f695f35a2b14023c9bef948084b820
myLessonss <izabayoeloi2@gmail.com>
2025-12-02 11:46:26 +0200

    Finalising Stock system

--- stock_admin/stock/Main.java
+++ stock_admin/stock/Main.java
@@ -1,10 +1,14 @@
 package stock;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner myscan=new Scanner(System.in);
+        //Object for stockItem
         StockItem myobj=new StockItem(null, null, null, 0, 0, 0, 0);
+        //Object for implementation methods
+        StockCalculationImplementation implement=new StockCalculationImplementation();
+        System.out.println("\n----------Enter in Item details------------");
+        //Item Code
         while (true) {
             try {
                 System.out.println("Enter in your item code");
@@ -12,54 +16,100 @@ public class Main {
                 myobj.setItemCode(itemCode);
                 break;
             } catch (IllegalArgumentException e) {
-                throw new IllegalArgumentException("Error: "+e.getMessage());
+                System.out.println("Error: "+e.getMessage());
             }
         }
+        //Item Name
         while (true) {
             try {
-                
-            } catch (Exception e) {
-                // TODO: handle exception
+                System.out.println("Enter in your item name");
+                String itemName=myscan.nextLine();
+                myobj.setItemName(itemName);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: "+e.getMessage());
+            }
+        //Item Category
+        }while (true) {
+            try {
+                System.out.println("Enter in Item Category");
+                String category=myscan.nextLine();
+                myobj.setCategory(category);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: "+e.getMessage());
             }
         }
-        /*while(true){
-                try {
-            //item code
-            System.out.println("Enter in your item code");
-            String itemCode=myscan.nextLine();
-            myobj.setItemCode(itemCode);
-            //item name
-            System.out.println("Enter in item name");
-            String name=myscan.nextLine();
-            myobj.setItemName(name);
-            //item category
-            System.out.println("Enter in Category");
-            String catg=myscan.nextLine();
-            myobj.setCategory(catg);
-            //quantity
-            System.out.println("Enter in Quantity");
-            int quant=myscan.nextInt();
-            myobj.setQuantity(quant);
-            //turnover amount
-            System.out.println("Enter in Turnover amount");
-            double turn=myscan.nextDouble();
-            myobj.setTurnoverAmount(turn);
-            //buying price
-            System.out.println("Enter in Buying price");
-            double buy=myscan.nextDouble();
-            myobj.setBuyingPrice(buy);
-            //selling price
-            System.out.println("Enter in Selling price");
-            double sell=myscan.nextDouble();
-            myobj.setSellingPrice(sell);
-
-
+        //quantity
+        while (true) {
+            try {
+                System.out.println("Enter in the quantity");
+                int quant=myscan.nextInt();
+                myscan.nextLine();
+                myobj.setQuantity(quant);
+                break;
             } catch (IllegalArgumentException e) {
-            System.out.println("Error: "+e.getMessage());
+                System.out.println("Error: "+e.getMessage());
+            } catch (java.util.InputMismatchException e){
+                System.out.println("Invalid input, Quantity must be Postive");
+                myscan.nextLine();
             }
-        }*/
-        
-        
-        
+        }
+        //turnover amount
+        while (true) {
+            try {
+                System.out.println("Enter in Turnover Amount");
+                double turnover=myscan.nextDouble();
+                myscan.nextLine();
+                myobj.setTurnoverAmount(turnover);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: "+e.getMessage());
+            } catch (java.util.InputMismatchException e){
+                System.out.println("Invalid input, please enter a Turnover Amount.");
+                myscan.nextLine();
+            }
+        }
+        //buying price
+        while (true) {
+            try {
+                System.out.println("Enter in your Buying Amount");
+                double buy=myscan.nextDouble();
+                myscan.nextLine();
+                myobj.setBuyingPrice(buy);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: "+e.getMessage());
+            } catch (java.util.InputMismatchException e){
+                System.out.println("Invalid Input, Enter in Buying Amount");
+                myscan.nextLine();
+            }
+        }
+        //selling price
+        while (true) {
+            try {
+                System.out.println("Enter in Selling amount");
+                double sell=myscan.nextDouble();
+                myscan.nextLine();
+                myobj.setSellingPrice(sell);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: "+e.getMessage());
+            } catch (java.util.InputMismatchException e){
+                System.out.println("Invalid Input, Enter in Selling Amount");
+                myscan.nextLine();
+            }
+        }
+        //OUTPUT
+        System.out.println("\n---------------Stock Calculation-----------------");
+        System.out.println("Item Code: "+myobj.getItemCode());
+        System.out.println("Item Name: "+myobj.getItemName());
+        System.out.println("Item Category: "+myobj.getCategory());
+        System.out.println("Item Quantity: "+myobj.getQuantity());
+        System.out.println("Item Turnover Amount: "+myobj.getTurnoverAmount());
+        System.out.println("VAT: "+implement.calculateVat(myobj.getTurnoverAmount()));
+        System.out.println("Markup profit: "+implement.markupProfit(myobj.getSellingPrice(), myobj.getBuyingPrice()));
+        System.out.println("Total Stock Value: "+implement.totalStockValue(myobj.getQuantity(),myobj.getSellingPrice()));
+        System.out.println("Restocking Fee: "+implement.restockingFee(myobj.getBuyingPrice()));
     }
 }
